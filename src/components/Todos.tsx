57f35d755d0879db9303f8f217a112bf9ac4f09a
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-03-19 13:10:10 +0100

    first commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,18 @@
+import React, {useContext} from "react";
+import classes from './Todos.module.css'
+import TodoList from "./TodoList";
+import { TodosContext } from "../store/todos-context";
+
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext)
+
+  return (
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => (
+        <TodoList key={item.id} text={item.text} onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)} />
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
